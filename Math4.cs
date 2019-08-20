bf49a249e187f67c8407b04bf6f86c7582724a8a
US <US.yurkaus@gmail.com>
2019-08-20 18:12:19 +0300

    add 4 and 5 tasks

---
+++ Math4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    private static void Main(string[] args)
+            {
+
+                int x = 3;
+                double e = Math.E;
+
+                double Numerator = 6 * Math.Log(Math.Sqrt(Math.Pow(e, x + 1) + Math.Pow(e, x) * Math.Cos(x)));
+                double Denominator = Math.Log(x - Math.Pow(e, x + 1) * Math.Sin(x));
+                double addition = Math.Abs(Math.Cos(x) / Math.Pow(e, Math.Sin(x)));
+                double Z = Numerator / Denominator + addition;
+
+                Console.WriteLine(Z);
+
+
+
+            }
+        }
+    }
+
+                  
+        
+  
+
