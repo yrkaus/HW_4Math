d7c3b8c8a5bf6b9c1fcafe01f3c50b6faa5c7df0
yrkaus <ceo@fx-private.com>
2019-08-20 18:15:13 +0300

    Update Math4.cs

--- Math4.cs
+++ Math4.cs
@@ -20,8 +20,52 @@ namespace ConsoleApp9
             }
         }
     }
+using System;
 
-                  
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RequestArray();
+        }
+        {
+        static int[] array = new int[0];
+            if (Array.Lenght != 0)
+            {
+                var result = array[0];
+            }
+            static int[] CetArray()
+            {
+                return null;
+            }
+            static void RequestArray()
+            {
+                try
+                {
+                    var array = GetArray();
+
+                    for (int i = 0; i < Array.Length; i++)
+                    {
+                        Console.WriteLine(array[i]);
+                    }
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+        finally
+        {
+            Console.WriteLine("End of function");
+        }
+            }
+
+
+        }
+    }
+}
+
         
   
 
